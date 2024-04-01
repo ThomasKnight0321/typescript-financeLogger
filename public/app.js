2c608241aa7e5ede5528d96f5d3d0e1c7f8f31b2
thomas2621910 <thomas@webglsoft.com>
2024-04-02 02:40:40 +0800

    typescript finance logger done

---
+++ public/app.js
@@ -0,0 +1,94 @@
+import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/Payment.js';
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs)
+// const invOne = new Invoice('mario', 'work on the mario website', 250);
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());  
+// })
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+});
+// generics
+// const addUID = <T extends {name: string}>(obj: T) => {
+//     let uid = Math.floor(Math.random() * 100);
+//     return {...obj, uid};
+// }
+// let docOne = addUID({ name: 'yoshi', age: 40 })
+// // let docTwo = addUID('hello')
+// console.log(docOne.age);
+// with interfaces
+// interface Resource<T> {
+//     uid: number;
+//     resourceName: string;
+//     data: T;
+// }
+// const docThree: Resource<object> = {
+//     uid: 1,
+//     resourceName: 'person',
+//     data: { name: 'shaun'}
+// }
+// const docFour: Resource<string[]> = {
+//     uid: 2,
+//     resourceName: 'shoppingList',
+//     data: ['bread', 'milk', 'toilet roll']
+// }
+// console.log(docThree, docFour); 
+// Enums
+// enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+// interface Resource<T> {
+//     uid: number;
+//     resourceType: number;
+//     data: T;
+// }
+// const doc1: Resource<object> = {
+//     uid: 1,
+//     resourceType: ResourceType.BOOK,
+//     data: { title: 'name of the wind' }
+// }
+// const doc2: Resource<object> = {
+//     uid: 10,
+//     resourceType: ResourceType.PERSON,
+//     data: { name: 'yoshi' }
+// }
+// console.log(doc1, doc2)
+// tuples
+// let arr = ['ryu', 25, true];
+// arr[0] = false;
+// arr[1] = 'yoshi';
+// arr = [30, false, 'yoshi']
+// let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = 'ken';
+// tup[1] = 30;
+// let student: [string, number];
+// student = ['chun-li', 223423];
